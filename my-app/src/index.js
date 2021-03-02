6c59fd551a74d3205b84da392a9373f56a6bf67d
yhanessaanne <77658704+yhanessaanne@users.noreply.github.com>
2021-03-01 18:10:11 -1000

    Task 4

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,15 +2,119 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import 'semantic-ui-css/semantic.min.css';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Menu, Icon, Dropdown, Image, Grid, Input, Button, List } from 'semantic-ui-react';
+
+class TopMenu extends React.Component {
+  render() {
+    return (
+        <Menu borderless className="topmenu">
+          <Container>
+            <Menu.Item fitted><Icon name="facebook f"/></Menu.Item>
+            <Menu.Item fitted><Icon name="twitter"/></Menu.Item>
+            <Menu.Item fitted><Icon name="pinterest"/></Menu.Item>
+            <Menu.Item fitted><Icon name="instagram"/></Menu.Item>
+            <Menu.Item fitted position="right"><Icon name="home"/></Menu.Item>
+            <Menu.Item fitted><Icon name="search"/></Menu.Item>
+            <Menu.Item fitted><Icon name="user"/></Menu.Item>
+            <Dropdown item text="MY CART 0" icon="shop">
+              <Dropdown.Menu>
+                <Dropdown.Item>My cart is currently empty.</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Container>
+        </Menu>
+    )
+  }
+}
+
+const logo = "http://courses.ics.hawaii.edu/ics314s21/morea/ui-frameworks/experience-islandsnow-logo.png"
+
+class IslandSnowLogo extends React.Component {
+  render() {
+    return (
+        <Image src={logo} centered/>
+    )
+  }
+}
+
+class MiddleMenu extends React.Component {
+  state = {}
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+    return (
+        <Menu horizontal borderless className="middlemenu">
+          <Grid centered container>
+            <Dropdown item text='MEN'/>
+            <Dropdown item text='WOMEN'/>
+            <Dropdown item text='KIDS'/>
+            <Dropdown item text='BRANDS'/>
+            <Menu.Item
+                name='SALE'
+                active={activeItem === 'SALE'}
+                onClick={this.handleItemClick}
+            >
+              SALE
+            </Menu.Item>
+          </Grid>
+        </Menu>
+    )
+  }
+}
+
+const middleImage = "http://courses.ics.hawaii.edu/ics314s21/morea/ui-frameworks/experience-islandsnow-video-snapshot.png";
+
+class FullWidthImage extends React.Component {
+  render() {
+    return (
+        <Image src={middleImage} fluid/>
+    )
+  }
+}
+
+class FooterMenu extends React.Component {
+  render() {
+    return (
+        <div className="footer-background">
+          <Grid centered container columns={3}>
+            <Grid.Column>
+              <List items={['About us', 'Store locations', 'Employment', 'Videos', 'Shipping & Returns', 'Terms & Conditions', 'Privacy Policy']} />
+            </Grid.Column>
+            <Grid.Column>
+              <List items={['Men', 'Women', 'Kids', 'Brands', 'Sales']} />
+            </Grid.Column>
+            <Grid.Column>
+              <List.Item>Join our mailing list for updates</List.Item>
+              <List.Item>Sign up to receive our email updates</List.Item>
+              <List.Item>
+                <Input action={{
+                  color: 'black',
+                  content: 'Join',
+                }}
+                       actionPosition='right'
+                       placeholder='Enter email address'
+                />
+              </List.Item>
+            </Grid.Column>
+          </Grid>
+        </div>
+    )
+  }
+}
 
 class IslandSnow extends React.Component {
 
   render() {
     return (
-        <Container textAlign="center">
-          <Header as='h1'>Island Snow!</Header>
-        </Container>
+        <div>
+          <TopMenu/>
+          <IslandSnowLogo/>
+          <MiddleMenu/>
+          <FullWidthImage/>
+          <FooterMenu/>
+        </div>
     );
   }
 }
